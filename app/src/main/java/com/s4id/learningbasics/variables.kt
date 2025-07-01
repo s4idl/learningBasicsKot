b7b83461f4654a30b7dcc45a7e907b142cd8ca9e
Said Lopez <slopezjr@ucol.mx>
2025-06-30 19:27:27 -0600

    first part

---
+++ app/src/main/java/com/s4id/learningbasics/variables.kt
@@ -0,0 +1,37 @@
+package com.s4id.learningbasics
+
+fun main(){
+    println("OPPPPP")
+
+    //Int
+    val age:Int = 30
+    var age2:Int = 19
+    age2 = 20
+
+    // Float
+    val floatExample:Float = 30.5f
+
+    //Double
+    //Long
+
+    //alfanumericos
+
+    //char
+    val charExample:Char = 's'
+
+    //string
+    val stringExample:String = "Hola aprendiendo"
+    val stringExample2: String = "Its going to rain"
+
+    //booolean
+
+    val booleanExample0:Boolean = true
+    val booleanExample1:Boolean = false
+
+    var stringConcatenada:String = "hOLA"
+    println(stringConcatenada)
+    stringConcatenada = "hOLA me llamo roy y $stringExample2 y tengo $age2 años"
+    println(stringConcatenada)
+
+    val example123:String = age.toString()
+}
