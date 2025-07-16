a9f03ad9a2518e050ee3a24017d787a06490037a
Said Lopez <slopezjr@ucol.mx>
2025-07-15 19:42:34 -0600

    first part

--- app/src/main/java/com/s4id/learningbasics/firstapp/FirstAppActivity.kt
+++ app/src/main/java/com/s4id/learningbasics/firstapp/FirstAppActivity.kt
@@ -1,15 +1,29 @@
 package com.s4id.learningbasics.firstapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.s4id.learningbasics.R
+import kotlin.math.log
 
 class FirstAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_app)
+        val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
+        val etName = findViewById<AppCompatEditText>(R.id.etName)
+
+
+        btnStart.setOnClickListener {
+            val name = etName.text.toString()
+            if (name.isNotEmpty()){
+             Log.i("Said", "Se presiono $name")
+        }
+    }
     }
 }
---
+++ app/src/main/java/com/s4id/learningbasics/firstapp/ResultActivity.kt
@@ -0,0 +1,17 @@
+package com.s4id.learningbasics.firstapp
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.s4id.learningbasics.R
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+        val tvResult = findViewById<TextView>(R.id.tvResult)
+    }
+}
