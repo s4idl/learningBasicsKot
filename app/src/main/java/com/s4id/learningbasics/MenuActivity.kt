a8c2d18200cec59a6f4d93f228b80a84bce23739
Said Lopez <slopezjr@ucol.mx>
2025-07-16 21:31:55 -0600

    first part

--- app/src/main/java/com/s4id/learningbasics/MenuActivity.kt
+++ app/src/main/java/com/s4id/learningbasics/MenuActivity.kt
@@ -9,13 +9,21 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.s4id.learningbasics.firstapp.FirstAppActivity
+import com.s4id.learningbasics.imccalculator.ImcCalculatorActivity
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
         val btnSaludApp  = findViewById<Button>(R.id.btnSaludApp)
+        val btnIMCApp  = findViewById<Button>(R.id.btnIMCApp)
         btnSaludApp.setOnClickListener{ navigateToSaludApp() }
+        btnIMCApp.setOnClickListener{ navigateToImcApp() }
+    }
+
+    private fun navigateToImcApp(){
+        val intent = Intent(this, ImcCalculatorActivity::class.java)
+        startActivity(intent)
     }
     private fun navigateToSaludApp(){
         val intent = Intent(this, FirstAppActivity::class.java)
---
+++ app/src/main/java/com/s4id/learningbasics/imccalculator/ImcCalculatorActivity.kt
@@ -0,0 +1,17 @@
+package com.s4id.learningbasics.imccalculator
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.s4id.learningbasics.R
+
+class ImcCalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imc_calculator)
+    }
+
+   
+}
